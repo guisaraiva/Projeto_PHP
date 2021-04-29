9e547adf3db4f839d9078e4db4d1284e14db63a7
Guilherme Saraiva <guilhermesilva93@hotmail.com>
2021-04-29 03:41:40 -0300

    Primeiros códigos em Php

--- exercicio.php
+++ exercicio.php
@@ -5,26 +5,25 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="recursos/css/estilo.css">
-    <title>Curso de PHP</title>
+    <link rel="stylesheet" href="recursos/css/exercicio.css">
+    <title>Exercício</title>
 </head>
-<body>
+<body class="exercicio">
 
     <header class="cabecalho">
         <h1>Curso de PHP</h1>
-        <h2>Índice dos Exercícios</h2>
+        <h2>Visualização do Exercício</h2>
     </header>
 
-    <main class="principal">
-        <div class="conteudo">
-            <nav class="modulos">
-                <div class="modulo verde">
-                    <h3>Módulo 01</h3>
-                    <ul>
-                        <li><a href="exercicio.php">Exercício A</a></li>
+    <nav class="navegacao">
+        <a href="#" class="verde"> Sem formatação </a>
+        <a href="index.php" class="vermelho">Voltar</a>
 
-                    </ul>
-                </div>
-           </nav>
+    </nav>
+    <main class="principal">
+
+        <div class="conteudo">
+
         </div>
     </main>
 
